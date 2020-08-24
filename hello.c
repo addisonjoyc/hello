2fa19c3383c34b245950381138deb802e0ea7de0
addisonjoyc <69826389+addisonjoyc@users.noreply.github.com>
2020-08-24 12:01:25 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Addison Cheek
+ *         acheek2@huskers.unl.edu
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Addison\n");
+
+  return 0;
+}
